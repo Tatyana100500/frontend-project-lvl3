90d363b54701822fd8231cf16eadb35d6fc5d473
Tatiana Chernishova <chernishovate@mail.ru>
2021-03-29 12:00:38 +0300

    step1

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
-install: install-deps
-
-install-deps:
+install:
 	npm ci
+
+build:
+	rm -rf dist
+	NODE_ENV=production npx webpack
+
+test:
+	npm test
 
 lint:
 	npx eslint .
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -10,8 +10,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
     ],
   },
